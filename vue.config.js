f5367212eb35220e73e14805dcda524b9fe28e31
GeneralRazz <130154058+GeneralRazz@users.noreply.github.com>
2026-01-22 12:52:01 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   // Use an array here for compatibility with older Vue CLI versions
+  publicPath: '/',
+  outputDir: './',
   transpileDependencies: [],
 
   publicPath: process.env.NODE_ENV === 'production'
