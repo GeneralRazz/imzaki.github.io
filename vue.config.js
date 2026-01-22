f99e169a174b7395ae89bd325c13dcd62512e10f
GeneralRazz <130154058+GeneralRazz@users.noreply.github.com>
2026-01-22 12:43:20 -0400

    Deploy Code

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Use an array here for compatibility with older Vue CLI versions
+  transpileDependencies: [],
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/'
+}
