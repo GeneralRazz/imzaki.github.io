e65c1480174f28271dff95823eadc293d36f623f
GeneralRazz <130154058+GeneralRazz@users.noreply.github.com>
2026-01-22 10:25:02 -0400

    edited env file and redid some text

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,13 +4,13 @@
 
     <div class="paragraph">
       <div>
-        I'm <strong>Zaki</strong>, a Dal Student in his last semester doing applied computer science and user experience design.<br/>
-        I spend my working on Uni stuff and being the in-game leader of Dal's Counter-Strike 2 team.
+        I'm <strong>Zaki</strong>, a final-year Applied Computer Science student at Dalhousie University specializing in User Experience Design.<br/>
+        I balance my time between advanced academic projects and serving as the In-Game Leader for Dalhousie’s Counter-Strike 2 varsity team
       </div>
 
-      <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">game projects</router-link>, on <router-link to="/other-projects">other uni stuff</router-link>, and did <router-link to="/resume">a bunch of other things</router-link> as well.</div>
+      <div style="margin-top: 20px;">I've been part of numerous <router-link to="/game-projects">game projects</router-link>, contributed to <router-link to="/other-projects">other uni projects</router-link>, and did <router-link to="/resume">a bunch of other things</router-link> as well.</div>
 
-      <div style="margin-top: 40px;">I'm <strong>looking land a job</strong> as a Gameplay Programmer, Game UX Designer/UI Programmer. You can reach me at <a href="mailto:mohammedzaksaj@gmail.com">mohammedzaksaj@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
+      <div style="margin-top: 40px;">I am currently <strong>seeking opportunities</strong> as a Gameplay Programmer, Game UX Designer/UI Programmer. You can reach me at <a href="mailto:mohammedzaksaj@gmail.com">mohammedzaksaj@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
     </div>
 
     <div class="photo">
